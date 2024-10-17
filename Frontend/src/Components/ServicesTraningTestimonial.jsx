af193b5000bb18cafedc6b01d831a82610828916
Shaurya Pratap Singh <shauryasinghad01@gmail.com>
2024-10-17 15:53:08 +0530

    updated service training testimonial

--- Frontend/src/Components/ServicesTraningTestimonial.jsx
+++ Frontend/src/Components/ServicesTraningTestimonial.jsx
@@ -29,8 +29,8 @@ function ServicesTraningTestimonial() {
         <div className="w-[80%] h-[40%] max-[601px]:w-full rounded-[20px] bg-white self-end p-[10px] flex flex-col gap-[20px] max-[601px]:gap-[10px] ">
           <div className="flex flex-row items-center ">
             <div className="flex flex-row items-center gap-[20px] max-[601px]:gap-[10px] ">
-              <div className="w-[35px] h-[35px] rounded-[50%] bg-[#D9D9D9] "></div>
-              <h3 className="text-[17px] max-[601px]:text-[15px] font-['Raleway'] font-semibold ">
+              {/* <div className="w-[35px] h-[35px] rounded-[50%] bg-[#D9D9D9] "></div> */}
+              <h3 className="text-[17px] pl-[10px] max-[601px]:text-[15px] font-['Raleway'] font-semibold ">
                 Anjali Sharma
               </h3>
             </div>
@@ -42,15 +42,16 @@ function ServicesTraningTestimonial() {
             />
           </div>
           <p className="font-['Raleway'] font-normal text-[15px] max-[601px]:text-[13px] ">
-            Ornate Makeup Studio transformed me into a vision of beauty on my
-            wedding day. Their attention to detail and artistry is unmatched!
+            From the moment I sat in the chair, I knew I was in good hands.
+            Ornate Makeup Studio worked wonders on my makeup, bringing my bridal
+            vision to life flawlessly!
           </p>
         </div>
         <div className="w-[80%] h-[40%] max-[601px]:w-full rounded-[20px] bg-white self-start p-[10px] flex flex-col gap-[20px] max-[601px]:gap-[10px] ">
           <div className="flex flex-row items-center ">
             <div className="flex flex-row items-center gap-[20px] max-[601px]:gap-[10px] ">
-              <div className="w-[35px] h-[35px] rounded-[50%] bg-[#D9D9D9] "></div>
-              <h3 className="text-[17px] max-[601px]:text-[15px] font-['Raleway'] font-semibold ">
+              {/* <div className="w-[35px] h-[35px] rounded-[50%] bg-[#D9D9D9] "></div> */}
+              <h3 className="text-[17px] pl-[10px] max-[601px]:text-[15px] font-['Raleway'] font-semibold ">
                 Priya Srivastava
               </h3>
             </div>
@@ -62,15 +63,16 @@ function ServicesTraningTestimonial() {
             />
           </div>
           <p className="font-['Raleway'] font-normal text-[15px] max-[601px]:text-[13px] ">
-            Ornate Makeup Studio transformed me into a vision of beauty on my
-            wedding day. Their attention to detail and artistry is unmatched!
+            I couldn't have asked for a better makeup artist on my big day.
+            Ornate Makeup Studio made me feel like the most beautiful version of
+            myself, and I was blown away by their skill and professionalism.
           </p>
         </div>
         <div className="w-[80%] h-[40%] max-[601px]:w-full rounded-[20px] bg-white self-end p-[10px] flex flex-col gap-[20px] max-[601px]:gap-[10px] ">
           <div className="flex flex-row items-center ">
             <div className="flex flex-row items-center gap-[20px] max-[601px]:gap-[10px] ">
-              <div className="w-[35px] h-[35px] rounded-[50%] bg-[#D9D9D9] "></div>
-              <h3 className="text-[17px] max-[601px]:text-[15px] font-['Raleway'] font-semibold ">
+              {/* <div className="w-[35px] h-[35px] rounded-[50%] bg-[#D9D9D9] "></div> */}
+              <h3 className="text-[17px] pl-[10px] max-[601px]:text-[15px] font-['Raleway'] font-semibold ">
                 Neha Tyagi
               </h3>
             </div>
@@ -82,8 +84,9 @@ function ServicesTraningTestimonial() {
             />
           </div>
           <p className="font-['Raleway'] font-normal text-[15px] max-[601px]:text-[13px] ">
-            Ornate Makeup Studio transformed me into a vision of beauty on my
-            wedding day. Their attention to detail and artistry is unmatched!
+            The team at Ornate Makeup Studio is incredible. They created a
+            stunning look for my reception that lasted all night and looked
+            perfect in every photo.
           </p>
         </div>
       </div>
